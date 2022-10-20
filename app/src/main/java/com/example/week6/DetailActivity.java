a241c8443da75c002c03058ea7b2b4c12ee4760b
newbornprograming <yohandeny.10@gmail.com>
2022-10-20 19:19:12 +0700

    perubahan setting harga pada detail activity

--- app/src/main/java/com/example/week6/DetailActivity.java
+++ app/src/main/java/com/example/week6/DetailActivity.java
@@ -10,9 +10,9 @@ import android.widget.TextView;
 public class DetailActivity extends AppCompatActivity {
 
     ImageView imgViewMakanan;
-    TextView txtViewJudul, txtViewisi;
+    TextView txtViewJudul, txtViewisi, txtViewHarga;
 
-    String namaMakanan, infoMakanan;
+    String namaMakanan, infoMakanan, hargaMakanan;
     int fotoMakanan;
 
     @Override
@@ -22,6 +22,7 @@ public class DetailActivity extends AppCompatActivity {
 
         txtViewJudul = findViewById(R.id.txtViewJudul);
         txtViewisi = findViewById(R.id.txtViewisi);
+        txtViewHarga = findViewById(R.id.txtViewHarga);
         imgViewMakanan = findViewById(R.id.imgViewMakanan);
 
         // foto makanan
@@ -35,12 +36,14 @@ public class DetailActivity extends AppCompatActivity {
     private void getInputExtra(){
         namaMakanan = getIntent().getStringExtra("nama_makanan");
         infoMakanan = getIntent().getStringExtra("info_makanan");
+        hargaMakanan= getIntent().getStringExtra("harga");
 
-        setDataActivity(namaMakanan, infoMakanan);
+        setDataActivity(namaMakanan, infoMakanan, hargaMakanan);
     }
 
-    private void setDataActivity(String namaMakanan, String infoMakanan){
+    private void setDataActivity(String namaMakanan, String infoMakanan, String hargaMakanan){
         txtViewJudul.setText(namaMakanan);
         txtViewisi.setText(infoMakanan);
+        txtViewHarga.setText(hargaMakanan);
     }
 }
